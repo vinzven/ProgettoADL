5a33609add855699873bc968f107a5f68129884e
assasinghira01 <scarvaglieri.antonio@studium.unict.it>
2026-02-28 16:44:40 +0100

    piccolo fix

--- modulo_C#/Modulo_C++/Modulo_C++.cpp
+++ modulo_C#/Modulo_C++/Modulo_C++.cpp
@@ -73,7 +73,7 @@ int main() {
             json risultato_finale;
             risultato_finale["ordinate_per_prezzo"] = offerte_prezzo;
             risultato_finale["ordinate_per_tempo"] = offerte_tempo;
-            risultato_finale["totale_offerte_analizzate"] = lista_originale.size();
+            risultato_finale["totale_offerte_analizzatee"] = lista_originale.size();
 
             // 5. STAMPA IL RISULTATO PER MANDARLO A C# (tramite Python)
             std::cout << risultato_finale.dump(4) << std::endl;
@@ -88,13 +88,3 @@ int main() {
 
     return 0;
 }
-// Per eseguire il programma: CTRL+F5 oppure Debug > Avvia senza eseguire debug
-// Per eseguire il debug del programma: F5 oppure Debug > Avvia debug
-
-// Suggerimenti per iniziare: 
-//   1. Usare la finestra Esplora soluzioni per aggiungere/gestire i file
-//   2. Usare la finestra Team Explorer per connettersi al controllo del codice sorgente
-//   3. Usare la finestra di output per visualizzare l'output di compilazione e altri messaggi
-//   4. Usare la finestra Elenco errori per visualizzare gli errori
-//   5. Passare a Progetto > Aggiungi nuovo elemento per creare nuovi file di codice oppure a Progetto > Aggiungi elemento esistente per aggiungere file di codice esistenti al progetto
-//   6. Per aprire di nuovo questo progetto in futuro, passare a File > Apri > Progetto e selezionare il file con estensione sln
